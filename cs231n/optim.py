951a2441482e376e4fa52a59d95bd2393e6a2088
Shawn94 <sheroz3898@gmail.com>
2020-05-06 17:43:14 +0000

    Minor updates to FullyConnectedNets

--- cs231n/optim.py
+++ cs231n/optim.py
@@ -65,7 +65,8 @@ def sgd_momentum(w, dw, config=None):
     # TODO: Implement the momentum update formula. Store the updated value in #
     # the next_w variable. You should also use and update the velocity v.     #
     ###########################################################################
-    pass
+    v = config['momentum'] * v - config['learning_rate'] * dw
+    next_w = w + v
     ###########################################################################
     #                             END OF YOUR CODE                            #
     ###########################################################################
@@ -99,7 +100,12 @@ def rmsprop(x, dx, config=None):
     # in the next_x variable. Don't forget to update cache value stored in    #
     # config['cache'].                                                        #
     ###########################################################################
-    pass
+    eps, learning_rate = config['epsilon'], config['learning_rate']
+    beta, v = config['decay_rate'], config['cache']
+    # RMSprop
+    v = beta * v + (1 - beta) * (dx * dx)        # exponential weighted average
+    next_x = x - learning_rate * dx / (np.sqrt(v) + eps)
+    config['cache'] = v
     ###########################################################################
     #                             END OF YOUR CODE                            #
     ###########################################################################
@@ -136,7 +142,18 @@ def adam(x, dx, config=None):
     # the next_x variable. Don't forget to update the m, v, and t variables   #
     # stored in config.                                                       #
     ###########################################################################
-    pass
+    eps, learning_rate = config['epsilon'], config['learning_rate']
+    beta1, beta2 = config['beta1'], config['beta2']
+    m, v, t = config['m'], config['v'], config['t']
+    # Adam
+    t = t + 1
+    m = beta1 * m + (1 - beta1) * dx          # momentum
+    mt = m / (1 - beta1**t)                   # bias correction
+    v = beta2 * v + (1 - beta2) * (dx * dx)   # RMSprop
+    vt = v / (1 - beta2**t)                   # bias correction
+    next_x = x - learning_rate * mt / (np.sqrt(vt) + eps)
+    # update values
+    config['m'], config['v'], config['t'] = m, v, t
     ###########################################################################
     #                             END OF YOUR CODE                            #
     ###########################################################################
